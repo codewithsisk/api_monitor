89e7d9303d41012244c0b26f144b0d2ddee6aecb
mrsisk <siskmusik@gmail.com>
2023-04-14 16:32:17 +0200

    draft

--- lib/api_monitor_web/live/invoice_live/index.ex
+++ lib/api_monitor_web/live/invoice_live/index.ex
@@ -8,14 +8,27 @@ defmodule ApiMonitorWeb.MonitorLive.Index do
     if connected?(socket) do
       Monitor.subscribe()
     end
-    {:ok, socket}
+    {:ok, assign(socket, form: %{url: ""}, data: [])}
   end
 
 
-  def handle_info({:message, payload}, socket) do
-    IO.inspect "Message recieved"
-    IO.inspect payload
+  def handle_event("validate", %{"url" => url}, socket) do
+    IO.inspect url
     {:noreply, socket}
+  end
+
+  def handle_event("save", %{"url" => url}, socket) do
+    IO.inspect "saving"
+    IO.inspect url
+    spawn(fn -> Monitor.request(url) end)
+    {:noreply, socket}
+  end
+
+
+  def handle_info({:message, {:ok, response}}, socket) do
+    IO.inspect "Message recieved"
+    IO.inspect response
+    {:noreply, assign(socket, data: [response | socket.assigns.data])}
   end
 
   end
